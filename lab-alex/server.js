fe962da5795489d25be29af2b5e5d9e0ca7717c6
warlordlizard <alexjbowers88@gmail.com>
2018-03-05 15:37:23 -0800

    finished server and auth middleware

--- lab-alex/server.js
+++ lab-alex/server.js
@@ -5,12 +5,25 @@ const debug = require(
   'debug'
 )('pictogram:server');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const authRouter = require('./route/auth-route.js');
+const errors = require('./lib/error-middleware.js');
+
+dotenv.load();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI);
 
-app.listen(PORT, () => {
+app.use(cors());
+app.use(morgan('dev'));
+app.use(authRouter);
+app.use(errors);
+
+const server = module.exports = app.listen(PORT, () => {
   debug(`server up on ${PORT}`);
 });
+server.isRunning = true;
