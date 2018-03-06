e8b961061534df8b8d355581451b2dfb5d76f939
warlordlizard <alexjbowers88@gmail.com>
2018-03-05 17:20:08 -0800

    done

--- lab-alex/lib/server-toggle.js
+++ lab-alex/lib/server-toggle.js
@@ -20,9 +20,9 @@ exports.serverOn = function(server, done) {
 exports.serverOff = function(server, done) {
   if (server.isRunning) {
     server.close( err => {
-      if (err) return done(err);
+      if (err) return done();
       server.isRunning = false;
-      mongoose.connection = false;
+      mongoose.connection.close();
       debug('server down!');
       done();
     });
