104d6d73368f334d27bf6f97a647be11d19a576d
warlordlizard <alexjbowers88@gmail.com>
2018-03-06 22:52:51 -0800

    routes and some tests written

--- lab-alex/model/collection.js
+++ lab-alex/model/collection.js
@@ -7,6 +7,7 @@ const collSchema = Schema({
   name: { type: String, required: true },
   desc: { type: String, required: true },
   created: { type: Date, required: true, default: Date.now },
+  userID: { type: Schema.Types.ObjectId, required: true },
 });
 
 module.exports = mongoose.model('collection', collSchema);
